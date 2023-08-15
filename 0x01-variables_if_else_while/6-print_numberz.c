c2a814111744fabe846d454b0be0af02b08d4217
Porpu <porpuotp@gmal.com>
2023-08-15 04:11:11 -0700

    C:/Program Files/Git/

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,16 +11,16 @@
 */
 int main(void)
 {
-putchar('0' + 0); /* Print the digit '0' */
-putchar('0' + 1); /* Print the digit '1' */
-putchar('0' + 2); /* Print the digit '2' */
-putchar('0' + 3); /* Print the digit '3' */
-putchar('0' + 4); /* Print the digit '4' */
-putchar('0' + 5); /* Print the digit '5' */
-putchar('0' + 6); /* Print the digit '6' */
-putchar('0' + 7); /* Print the digit '7' */
-putchar('0' + 8); /* Print the digit '8' */
-putchar('0' + 9); /* Print the digit '9' */
+putchar('0'); /* Print the digit '0' */
+putchar('1'); /* Print the digit '1' */
+putchar('2'); /* Print the digit '2' */
+putchar('3'); /* Print the digit '3' */
+putchar('4'); /* Print the digit '4' */
+putchar('5'); /* Print the digit '5' */
+putchar('6'); /* Print the digit '6' */
+putchar('7'); /* Print the digit '7' */
+putchar('8'); /* Print the digit '8' */
+putchar('9'); /* Print the digit '9' */
 putchar('\n');    /* Print a newline character */
 
 return (0); /**< Return 0 to indicate successful execution */
