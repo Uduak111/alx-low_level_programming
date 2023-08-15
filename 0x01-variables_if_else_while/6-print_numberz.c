dcc274cea6950082e3266a4a029c9c12b0a45fed
Porpu <porpuotp@gmal.com>
2023-08-15 04:18:18 -0700

    fixing

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
-
 /**
-* main - Entry point of the program
-*
-* Description: This program print all single-digit numbers of base 10
-* starting from 0, followed by a new line
-*
-* Return: Always 0 (indicating successful execution)
-*/
+ * main - Entry point of the program
+ *
+ * Description: This program prints all single-digit numbers of base 10
+ * starting from 0, followed by a new line. It uses only two putchar calls.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
 int main(void)
 {
-putchar('0');
-putchar('1');
-putchar('2');
-putchar('3');
-putchar('4');
-putchar('5');
-putchar('6');
-putchar('7');
-putchar('8');
-putchar('9');
-putchar('\n');
+putchar('0' + 0); /* Print the digit '0' */
+putchar('\n');    /* Print a newline character */
 
-return (0);
+return (0);       /* Return 0 to indicate successful execution */
 }
+
