47ac044e321d99e98c46fcd856aa23fbd4107516
uduak <uduakj266@gmail.com>
2023-08-15 03:37:20 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point of the program
+*
+* Description: This program print all single-digit numbers of base 10
+* starting from 0, followed by a new line
+*
+* Return: Always 0 (indicating successful execution)
+*/
+int main(void)
+{
+putchar('0' + 0); /* Print the digit '0' */
+putchar('0' + 1); /* Print the digit '1' */
+putchar('0' + 2); /* Print the digit '2' */
+putchar('0' + 3); /* Print the digit '3' */
+putchar('0' + 4); /* Print the digit '4' */
+putchar('0' + 5); /* Print the digit '5' */
+putchar('0' + 6); /* Print the digit '6' */
+putchar('0' + 7); /* Print the digit '7' */
+putchar('0' + 8); /* Print the digit '8' */
+putchar('0' + 9); /* Print the digit '9' */
+putchar('\n');    /* Print a newline character */
+
+return (0); /**< Return 0 to indicate successful execution */
