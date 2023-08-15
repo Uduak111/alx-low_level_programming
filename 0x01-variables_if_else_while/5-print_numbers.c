5eb2237735288a00e236840bb66c78b3f31b906a
uduak <uduakj266@gmail.com>
2023-08-15 03:18:11 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all single-digit numbers of base 10
+ *              starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
 int main(void)
 {
-int number;  /* Declare a variable to store the current number */
+int number; /**< Variable to store the current number */
 
-/* Loop to iterate through single-digit numbers (0 to 9) */
+    /**< Loop to iterate through single-digit numbers (0 to 9) */
 for (number = 0; number < 10; number++)
 {
-putchar(number + '0');  /* Print the current number as a character */
+putchar(number + '0'); /**< Print the current number as a character */
 }
 
-putchar('\n');  /* Print a newline character to start a new line */
+putchar('\n'); /**< Print a newline character to start a new line */
 
-return (0);  /* Return 0 to indicate successful execution */
+return (0); /**< Return 0 to indicate successful execution */
 }
 
