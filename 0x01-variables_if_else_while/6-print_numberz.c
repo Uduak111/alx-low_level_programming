ca6aea192430a607f66d7c4b70c9ead65a0ce7bc
Porpu <porpuotp@gmal.com>
2023-08-15 06:39:44 -0700

    commits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,7 @@
  * main - Entry point of the program
  *
  * Description: This program prints all single-digit numbers of base 10
- *              starting from 0, followed by a new line. It uses only two putchar calls.
+ * starting from 0, followed by a new line. It uses only two putchar calls.
  *
  * Return: Always 0 (indicating successful execution)
  */
@@ -16,6 +16,7 @@ for (x = 0; x <= 9; x++)
 {
 putchar(x + '0');
 }
+putchar('\n'); // Print a newline character after the loop
 return (0);
 }
 
