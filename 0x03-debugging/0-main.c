e76cfa7a9e6aa4eeabfe4edec96025376af34587
Porpu <porpuotp@gmal.com>
2023-08-17 16:07:41 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+* main - test function that print if integer is pasitive, negative, or zero
+* Return: 0
+* */
+
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive or negative(i);
+
+
+return (0);
+}
