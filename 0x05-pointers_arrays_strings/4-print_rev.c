604d24d55c0ff83ab3e9e9244d8c30ae8339d12c
Porpu <porpuotp@gmal.com>
2023-08-21 13:39:27 -0700

    print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h" /* Make sure to include any necessary header files */
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line.
+ * @s: Pointer to the string to be printed in reverse.
+ */
+void print_rev(char *s)
+{
+int length = 0;
+
+/* Calculate the length of the string */
+while (s[length] != '\0')
+{
+length++;
+}
+
+/* Print the string in reverse order */
+for (int i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+
+_putchar('\n'); /* Print a new line after the reversed string */
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ void swap_int(int *a, int *b);
 int _putchar(char c); /*Prototype for _putchar function if needed */
 int _strlen(char *s);
 int _strlen(char *s);
+void print_rev(char *s);
 
 /*Add more function here if needed */
 
