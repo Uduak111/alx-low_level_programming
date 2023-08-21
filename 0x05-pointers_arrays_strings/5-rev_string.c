1c020df27a250ba9d1bba9503d58010258258d4c
Porpu <porpuotp@gmal.com>
2023-08-21 14:01:04 -0700

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,25 @@
 void rev_string(char *s)
 {
 int length = 0;
-    
+
 /* Calculate the length of the string */
 while (s[length] != '\0')
 {
 length++;
 }
-    
-int start = 0;
-int end = length - 1;
+int start, end; /* Declare the variables at the beginning of the block */
 char temp;
-    
+
+start = 0; /* Initialize the variables after their declarations */
+end = length - 1;
+
 /* Swap characters from start to end to reverse the string */
 while (start < end)
 {
 temp = s[start];
 s[start] = s[end];
 s[end] = temp;
-        
+
 start++;
 end--;
 }
