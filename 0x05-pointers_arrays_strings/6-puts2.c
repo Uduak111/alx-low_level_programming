a577bed09840a3fa01881321de2c7d5ac28d66e2
Porpu <porpuotp@gmal.com>
2023-08-21 15:14:58 -0700

    back

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -18,12 +18,4 @@ i += 2; /* Move to the next character with a step of 2 */
 _putchar('\n'); /* Print a new line after printing characters */
 }
 
-int main(void)
-{
-char input[] = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.Hletn";
-
-puts2(input);
-
-return 0;
-}
 
