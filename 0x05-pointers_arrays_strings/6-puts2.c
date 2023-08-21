8147f52063cc8b03afd62ee539b696cdaf2ddfcc
Porpu <porpuotp@gmal.com>
2023-08-21 15:21:17 -0700

    C:/Program Files/Git/

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,11 @@ int i = 0;
 
 while (str[i] != '\0')
 {
-_putchar(str[i]); /* Print the current character */
+_putchar("%c", str[i]); /* Print the current character */
 
 i += 2; /* Move to the next character with a step of 2 */
 }
 
 _putchar('\n'); /* Print a new line after printing characters */
 }
-
 
