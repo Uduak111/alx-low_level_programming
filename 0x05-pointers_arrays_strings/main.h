2b5e4d2e73b07b77e9f0aca5bfda1a4f73da69bf
Porpu <porpuotp@gmal.com>
2023-08-21 13:02:43 -0700

    swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,11 @@
 
 
 void reset_to_98(int *n);
-int _putchar(char c); /*Prototype for _putchar function if needed*/
+void swap_int(int *a, int *b);
+int _putchar(char c); /*Prototype for _putchar function if needed */
+
+
+/*Add more function here if needed */
 
 
 #endif /* MAIN_H */
