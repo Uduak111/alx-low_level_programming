3d9002232463d12979c5362400c94226fab3d583
Porpu <porpuotp@gmal.com>
2023-08-21 13:16:34 -0700

    strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h" // Make sure to include any necessary header files
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the input string.
+ * Return: Length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (s[length] != '\0')
+{
+length++;
+}
+
+return length;
+}
+
