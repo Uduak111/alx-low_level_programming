197a3678759dea31b601d68edc52c797315bfcb8
Porpu <porpuotp@gmal.com>
2023-08-21 20:07:28 -0700

    jjj

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"  /* Include necessary headers */
+#include <limits.h> /* Include the limits.h header */
 
 /**
  * _atoi - Converts a string to an integer.
@@ -7,37 +8,39 @@
  */
 int _atoi(char *s)
 {
-int result = 0;
-int sign = 1;
+    int result = 0;
+    int sign = 1;  /* Initialize sign as positive */
 
+    /* Skip leading whitespace characters */
+    while (*s == ' ' || (*s >= 9 && *s <= 13))
+    {
+        s++;
+    }
 
-while (*s == ' ' || (*s >= 9 && *s <= 13))
-{
-s++;
-}
+    /* Handle signs */
+    if (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+	{
+            sign = -1;
+        }
+        s++;
+    }
 
+    /* Convert characters to integer */
+    while (*s >= '0' && *s <= '9')
+    {
+        /* Check for overflow */
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && *s - '0' > INT_MAX % 10))
+	{
+            /* Handle overflow, return INT_MAX or INT_MIN based on the sign */
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
 
-if (*s == '-' || *s == '+')
-{
-if (*s == '-')
-{
-sign = -1;
-}
-s++;
-}
+        result = result * 10 + (*s - '0');
+        s++;
+    }
 
-
-while (*s >= '0' && *s <= '9') {
-
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && *s - '0' > INT_MAX % 10))
-{
-return (sign == 1) ? INT_MAX : INT_MIN;
-}
-
-result = result * 10 + (*s - '0');
-s++;
-}
-
-return result * sign;
+    return result * sign;
 }
 
