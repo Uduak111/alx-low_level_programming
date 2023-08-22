33f3e24040d72307727c0e283e84816791602b37
Porpu <porpuotp@gmal.com>
2023-08-21 17:22:50 -0700

    ready

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,25 +7,23 @@
 void puts_half(char *str)
 {
     int length = 0;
-
+    
     /* Calculate the length of the string */
     while (str[length] != '\0')
-{
+    {
         length++;
-}
+    }
 
-    /* Calculate the starting index for printing */
-{
+    /* Declare variables at the beginning of the block */
     int start = (length - 1) / 2 + 1;
-}
-
+    
     /* Print the characters starting from the calculated index */
     while (str[start] != '\0')
-{
+    {
         _putchar(str[start]);
         start++;
-}
-
+    }
+    
     _putchar('\n'); /* Print a new line after printing characters */
 }
 
