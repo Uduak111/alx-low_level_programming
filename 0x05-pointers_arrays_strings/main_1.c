8b9bbfa440e10bae0b58832f5262b654308b4ba1
Porpu <porpuotp@gmal.com>
2023-08-21 19:45:38 -0700

    C:/Program Files/Git/

---
+++ 0x05-pointers_arrays_strings/main_1.c
@@ -0,0 +1,10 @@
+#include "main.h" /* Include necessary header files */
+
+int main(void)
+{
+char str[] = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.";
+puts2(str);
+
+return (0);
+}
+
