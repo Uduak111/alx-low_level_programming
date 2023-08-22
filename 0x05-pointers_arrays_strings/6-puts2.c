77985ba14f7d6b8c93518d12c7f03ea9c767a5f1
Porpu <porpuotp@gmal.com>
2023-08-21 18:34:53 -0700

    C:/Program Files/Git/

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 int (i);
 
-for (i = 0; str[1]; i != '\0'; i++)
+for (i = 0; str[i]; != '\0'; i++)
 {
 if (i % 2 == 0)
 {
-_putchar(str[1]);
+_putchar(str[i]);
 }
 }
 _putchar('\n');
