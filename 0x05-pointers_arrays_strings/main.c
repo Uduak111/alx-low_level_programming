6e7204e31a0f5b8b797724b7db2eeb3371302ad9
Porpu <porpuotp@gmal.com>
2023-08-21 19:42:24 -0700

    finished

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,10 +1,9 @@
 #include "main.h" /* Include necessary header files */
 
-int main()
+int main(void)
 {
-char input[] = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.Hletn";
-
-puts2(input);
+char str[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+puts2(str);
 
 return (0);
 }
