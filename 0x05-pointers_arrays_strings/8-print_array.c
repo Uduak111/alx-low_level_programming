c7eb506cca4f2e1f5e6c92257954ba540253700c
Porpu <porpuotp@gmal.com>
2023-08-21 18:56:51 -0700

    done

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h> /* Include the necessary header file for printf */
 
 /**
- * print_array - Prints n elements of an array of integers followed by a new line.
+ * print_array Prints elements of an array of integers followed by a new line.
  * @a: Pointer to the array of integers.
  * @n: Number of elements to be printed.
  */
