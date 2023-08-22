a9c62473074c092e436035065c87f6bbbc3957fd
Porpu <porpuotp@gmal.com>
2023-08-21 19:20:14 -0700

    ..

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
-* puts2 - Prints every other character of a string followed by a new line.
-* @str: Pointer to the string to be printed.
-* Return: every other character
-*/
-
+ * puts2 - Prints every other character of a string followed by a new line.
+ * @str: Pointer to the string to be printed.
+ * Return: void
+ */
 void puts2(char *str)
 {
-int(i);
+int i;
 
 for (i = 0; str[i] != '\0'; i += 2)
 {
-if (i % 2 == 0)
-{
 _putchar(str[i]);
-}
 }
 _putchar('\n');
 }
+
+int main(void)
+{
+char str[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+puts2(str);
+
+return (0);
+}
+
