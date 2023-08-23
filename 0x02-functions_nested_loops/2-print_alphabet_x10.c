beedce45781ace4f7dc3e9bfa959409ca6f7ee36
Porpu <porpuotp@gmal.com>
2023-08-23 10:15:38 -0700

    ff

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet 10 times.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void print_alphabet_x10(void)
+int main(void)
 {
-char letter;
-
-for (int i = 0; i < 10; i++)
-{
-letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
+	print_alphabet_x10();
+	return (0);
 }
-_putchar('\n');
-}
-}
-
