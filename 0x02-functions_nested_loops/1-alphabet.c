ac4c75498af55625d48e475dc05d57405568b2f1
Porpu <porpuotp@gmal.com>
2023-08-23 09:43:51 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,15 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (file descriptor 1).
+ * @c: The character to be written.
+ * Return: On success, the number of characters written.
+ *         On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 
 /**
  * print_alphabet - Prints the lowercase alphabet followed by a new line.
