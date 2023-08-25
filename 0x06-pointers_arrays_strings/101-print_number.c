2a05386e18a31bba4153a8b08ca6ae05206eeb53
Porpu <porpuotp@gmal.com>
2023-08-25 08:54:38 -0700

    print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer using _putchar.
- * @n: The integer to be printed.
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-int divisor = 1;
-
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
 
-while (n / divisor >= 10)
+if (n / 10 != 0)
 {
-divisor *= 10;
+print_number(n / 10);
 }
 
-while (divisor > 0)
-{
-_putchar((n / divisor) % 10 + '0');
-divisor /= 10;
-}
+_putchar(n % 10 + '0');
 }
 
