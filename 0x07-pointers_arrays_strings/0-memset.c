430527dde84515a1efa335eddfd9bb1c30a091b2
Uduak <uduakj266@gmail.com>
2023-08-28 17:13:48 -0400

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,32 +1,19 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to write
- *
- * Return: On success 1. On error, -1 is returned.
- */
-int _putchar(char c);
-
-/**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
+void simple_print_buffer(char *buffer, unsigned int size) {
     unsigned int i;
 
-    for (i = 0; i < size; i++)
-    {
-        if (i % 10 == 0 && i != 0)
-        {
+    for (i = 0; i < size; i++) {
+        if (i % 10 == 0 && i != 0) {
             _putchar('\n');
-        }
-        else if (i != 0)
-        {
+        } else if (i != 0) {
             _putchar(' ');
         }
 
@@ -38,47 +25,5 @@ void simple_print_buffer(char *buffer, unsigned int size)
         _putchar(hex_digits[buffer[i] & 0xF]);
     }
     _putchar('\n');
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('-');
-    _putchar('\n');
-    simple_print_buffer(buffer, 98);
-    return (0);
-}
-
-char *_memset(char *s, char b, unsigned int n)
-{
-    unsigned int i;
-
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-    return s;
 }
 
