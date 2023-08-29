38cc68f5ba37a1f0306fca743832c722535d3b1d
Uduak <uduakj266@gmail.com>
2023-08-29 17:39:41 -0400

    correction

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,9 @@
 void print_alphabet(void);
 /* Function declaration for _putchar */
 void _putchar(char c);
+/* Function declaration for print_alphabet_x10 */
 void print_alphabet_x10(void);
+
 /* ... other declarations or includes ... */
 
 
