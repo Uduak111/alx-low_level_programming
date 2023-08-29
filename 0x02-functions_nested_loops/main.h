d475335eb2dd42ff49cf33c3d169c3873a710b82
Uduak <uduakj266@gmail.com>
2023-08-29 17:53:57 -0400

    isalpha.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,8 @@ void _putchar(char c);
 void print_alphabet_x10(void);
 /* Function declaration for int _islower */
 int _islower(int c);
+/* Function declaration for int _isalpha */
+int _isalpha(int c);
 
 /* ... other declarations or includes ... */
 
