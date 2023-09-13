ac5f0eb2fa512c62ffdbf5215da3e988c5eb7346
Uduak <uduakj266@gmail.com>
2023-09-12 21:40:57 -0400

    /

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
+/**
+ * @brief This is the main function of the program.
+ *
+ * This function prints the message "2-main.c" to the console.
+ *
+ * @return 0 indicating successful execution.
+ */
+int main(void)
 {
-		
-	printf("2-main.c\n");
-	return 0;
+printf("2-main.c\n");
+return (0);
 }
+
